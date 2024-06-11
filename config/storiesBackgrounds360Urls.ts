2a673b47884ae090440069de09024122ded5aeea
matheusaragaofs <matheusaragaofs@gmail.com>
2024-06-11 08:45:00 -0300

    Update URLs in storiesBackgrounds360Urls.ts

--- config/storiesBackgrounds360Urls.ts
+++ config/storiesBackgrounds360Urls.ts
@@ -6,13 +6,13 @@ export const storiesBackgrounds360Urls = {
     '2': 'https://kuula.co/share/5whbc?logo=1&info=1&fs=0&vr=0&zoom=1&thumbs=1&alpha=0.60inst=0inst=0&keys=0',
   },
   fantasy: {
-    '0': 'https://kuula.co/share/5wLMk?logo=1&info=1&fs=0&vr=0&zoom=1&thumbs=1', // 1
-    '1': 'https://kuula.co/share/5wLMR?logo=1&info=1&fs=0&vr=0&zoom=1&thumbs=1', // 2
-    '2': 'https://kuula.co/share/5wL2L?logo=1&info=1&fs=0&vr=0&zoom=1&thumbs=1', // 3.1
-    '3': 'https://kuula.co/share/5wLx1?logo=1&info=1&fs=0&vr=0&zoom=1&thumbs=1', // 3.2
-    '4': 'https://kuula.co/share/5wL8r?logo=1&info=1&fs=1&vr=0&zoom=1&thumbs=1', // 4
-    '5': 'https://kuula.co/share/5wL2W?logo=1&info=1&fs=0&vr=0&zoom=1&thumbs=1',
-    '6':'https://kuula.co/share/5wLVN?logo=1&info=1&fs=0&vr=0&zoom=1&thumbs=1'
+    '0': 'https://kuula.co/share/5wLMk?logo=1&info=1&fs=0&vr=0&zoom=1&thumbs=1&alpha=0.60&inst=0&keys=0', // 1
+    '1': 'https://kuula.co/share/5wLMR?logo=1&info=1&fs=0&vr=0&zoom=1&thumbs=1&alpha=0.60&inst=0&keys=0', // 2
+    '2': 'https://kuula.co/share/5wL2L?logo=1&info=1&fs=0&vr=0&zoom=1&thumbs=1&alpha=0.60&inst=0&keys=0', // 3.1
+    '3': 'https://kuula.co/share/5wLx1?logo=1&info=1&fs=0&vr=0&zoom=1&thumbs=1&alpha=0.60&inst=0&keys=0', // 3.2
+    '4': 'https://kuula.co/share/5wL8r?logo=1&info=1&fs=0&vr=0&zoom=1&thumbs=1&alpha=0.60&inst=0&keys=0', // 4
+    '5': 'https://kuula.co/share/5wL2W?logo=1&info=1&fs=0&vr=0&zoom=1&thumbs=1&alpha=0.60&inst=0&keys=0',
+    '6':'https://kuula.co/share/5wLVN?logo=1&info=1&fs=0&vr=0&zoom=1&thumbs=1&alpha=0.60&inst=0&keys=0'
   },
   action: {
     '0': '',
