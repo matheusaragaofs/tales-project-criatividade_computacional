155039cf54239dd0d40638976d68dc295915f520
matheusaragaofs <matheusaragaofs@gmail.com>
2024-06-10 21:23:37 -0300

    Update horror storiesBackgrounds360Urls in config/storiesBackgrounds360Urls.ts

--- config/storiesBackgrounds360Urls.ts
+++ config/storiesBackgrounds360Urls.ts
@@ -1,9 +1,9 @@
 
 export const storiesBackgrounds360Urls = {
   horror: {
-    '0': 'https://kuula.co/share/5w5QS?logo=1&info=1&fs=1&vr=0&zoom=1&sd=1&thumbs=1&inst=0',
-    '1': 'https://kuula.co/share/5whvp?logo=1&info=1&fs=1&vr=0&zoom=1&thumbs=1&inst=0',
-    '2': 'https://kuula.co/share/5whbc?logo=1&info=1&fs=1&vr=0&zoom=1&thumbs=1&inst=0',
+    '0': 'https://kuula.co/share/5w5QS?logo=1&info=1&fs=0&vr=0&zoom=1&thumbs=1&alpha=0.60&inst=0&keys=0',
+    '1': 'https://kuula.co/share/5whvp?logo=1&info=1&fs=0&vr=0&zoom=1&thumbs=1&alpha=0.60&inst=0&keys=0',
+    '2': 'https://kuula.co/share/5whbc?logo=1&info=1&fs=0&vr=0&zoom=1&thumbs=1&alpha=0.60inst=0inst=0&keys=0',
   },
   fantasy: {
     '0': '',
